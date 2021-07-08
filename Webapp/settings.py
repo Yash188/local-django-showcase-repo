909720086173e6a9b54257683446c63e1663cb03
Yash188 <yashgohel188@gmail.com>
2021-07-08 15:49:02 +0530

    Init Commit 7

--- Webapp/settings.py
+++ Webapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '5@1n(a5_yk&5rx)u2v!1a=85uq9vvl0+!c=3swr4s%jab5*je8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','django-admin-panel.herokuapp.com']
 
